5994f9df4979f1fe4c761e9219476c7bfaf38f03
Andy H <halsandr@hotmail.co.uk>
2017-06-11 21:10:28 +0100

    add jquery localy

--- web/index.php
+++ web/index.php
@@ -16,54 +16,57 @@ $sql = "SELECT * FROM `radio_stations` ORDER BY `radio_stations`.`pretty_name` A
 $result = $conn->query($sql);
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
+    <!DOCTYPE html>
+    <html lang="en">
 
-<head>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="apple-touch-icon" sizes="180x180" href="images/icons/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="images/icons/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="images/icons/favicon-16x16.png">
-    <link rel="manifest" href="images/icons/manifest.json">
-    <link rel="mask-icon" href="images/icons/safari-pinned-tab.svg" color="#5bbad5">
-    <link rel="shortcut icon" href="images/icons/favicon.ico">
-    <meta name="msapplication-config" content="images/icons/browserconfig.xml">
-    <meta name="theme-color" content="#ffffff">
-    <title>Pi-Radio</title>
-    <link href="css/main.css" rel="stylesheet">
-    <script defer src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
-    <script defer src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-    <script defer src="update.js"></script>
-</head>
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="apple-touch-icon" sizes="180x180" href="images/icons/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="images/icons/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="images/icons/favicon-16x16.png">
+        <link rel="manifest" href="images/icons/manifest.json">
+        <link rel="mask-icon" href="images/icons/safari-pinned-tab.svg" color="#5bbad5">
+        <link rel="shortcut icon" href="images/icons/favicon.ico">
+        <meta name="msapplication-config" content="images/icons/browserconfig.xml">
+        <meta name="theme-color" content="#ffffff">
+        <title>Pi-Radio</title>
+        <link href="css/main.css" rel="stylesheet">
+    </head>
 
-<body>
-    <div class="container">
-        <div class="row">
-            <div class="col-xs-12">
-                <H1>
-                    <img class="pi-icon" src="images/icons/rpi.svg">  Pi-Radio
-                </H1>
+    <body>
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-12">
+                    <H1>
+                        <img class="pi-icon" src="images/icons/rpi.svg"> Pi-Radio
+                    </H1>
+                </div>
             </div>
-        </div>
-        <div class="row">
-            <ul class="list-group clearfix">
-                
-                <?php while($row = $result->fetch_assoc()) { ?>
-                
-                <li class="list-group-item"><?php echo $row["pretty_name"] ;?>
-                    <button class="btn btn-default pull-right button-stop" type="button">
+            <div class="row">
+                <ul class="list-group clearfix">
+
+                    <?php while($row = $result->fetch_assoc()) { ?>
+
+                    <li class="list-group-item">
+                        <?php echo $row["pretty_name"] ;?>
+                        <button class="btn btn-default pull-right button-stop" type="button">
                         <i class="fa fa-stop"></i>
                     </button>
-                    <button onclick="playRadio(<?php echo $row["id"] ;?>)" class="player btn btn-<?php if($row["playing"]){echo "success";}else{echo "default";} ?> pull-right" type="button">
+                        <button onclick="playRadio(<?php echo $row[" id "] ;?>)" class="player btn btn-<?php if($row[" playing "]){echo "success ";}else{echo "default ";} ?> pull-right" type="button">
                         <i class="fa fa-play"></i>
                     </button>
-                </li>
-                
-                <?php } ;?>
-            </ul>
-                        
-        </div>
-    </div>
-</body>
+                    </li>
 
-</html>
+                    <?php } ;?>
+                </ul>
+
+            </div>
+        </div>
+
+
+        <script defer src="js/jquery-3.2.1.min.js"></script>
+        <script defer src="update.js"></script>
+
+    </body>
+
+    </html>
--- web/update.js
+++ web/update.js
@@ -1,15 +1,17 @@
-$(document).ready(function(){
-    $('.button-stop').click(function(){
-         $.ajax({
-               type: "GET",
-               url: "stop.php"
-             })
+$(document).ready(function () {
+    'use strict';
+    $('.button-stop').click(function () {
+        $.ajax({
+            type: "GET",
+            url: "stop.php"
+        });
         $('.player').removeClass('btn-success');
         $('.player').addClass('btn-default');
     });
 });
 
 function playRadio(stationID) {
+    'use strict';
     $.ajax({
         type: "POST",
         data: {
@@ -18,11 +20,11 @@ function playRadio(stationID) {
         url: "play.php",
         async: true
     });
-
 }
 
-$(document).ready(function(){
-    $('.player').click(function(){
+$(document).ready(function () {
+    'use strict';
+    $('.player').click(function () {
         $('.player').removeClass('btn-success');
         $('.player').addClass('btn-default');
         $(this).removeClass('btn-default');
